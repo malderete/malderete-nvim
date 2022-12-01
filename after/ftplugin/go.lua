b6315335149ccee6f4b1d6b91212c4d943553ef5
Martin Alderete <malderete@gmail.com>
2022-12-01 10:38:16 +0100

    Added indentation per filetype using ftplugin

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,7 @@
+-- tabs & indentation
+-- -- Default
+vim.opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+vim.opt.softtabstop = 4 -- 4 spaces
+vim.opt.shiftwidth = 4 -- 4 spaces for indent width
+vim.opt.expandtab = false -- no expand tabs
+vim.opt.autoindent = true -- copy indent from current line when starting new one
---
+++ after/ftplugin/python.lua
@@ -0,0 +1,8 @@
+-- tabs & indentation
+-- -- Default
+vim.opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+vim.opt.softtabstop = 4 -- 4 spaces
+vim.opt.shiftwidth = 4 -- 4 spaces for indent width
+vim.opt.expandtab = true -- expand tabs
+vim.opt.autoindent = true -- copy indent from current line when starting new one
+vim.opt.colorcolumn = "120"
