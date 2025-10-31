462131034c51ed3e21ea7e4eb4c97bd2e2865de3
Martin Alderete <malderete@gmail.com>
2025-10-31 15:43:26 +0100

    Tokyonight as colorscheme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,4 +1,4 @@
-local status, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
+local status, _ = pcall(vim.cmd, 'colorscheme tokyonight-night')
 if not status then
     print("Colorscheme not found!")
     return
